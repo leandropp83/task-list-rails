54aad3f42f95ccf43d62a9ffb97383cebb108d52
leandropp83 <leandro.pierin.lp@gmail.com>
2022-09-09 14:02:50 -0300

    Ajuste rotas tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "projects#index"
   resource :projects, only: [:create, :update, :destroy]
-  resource :tasks, only: [:create, :update]
-  get "projects/:id/destroy", to:"projects#destroy", as: :destroy
+  resource :tasks, only: [:create, :update, :destroy]
+  get "projects/:id/destroy", to:"projects#destroy", as: :destroy_projects
+  get "tasks/:id/destroy", to:"tasks#destroy", as: :destroy_tasks
 end
